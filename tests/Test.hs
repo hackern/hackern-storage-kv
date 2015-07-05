e697b5eb9c228e892973066a2a750b682db962d7
Zhen Zhang <izgzhen@gmail.com>
2015-07-05 21:08:55 +0800

    better test

--- tests/Test.hs
+++ tests/Test.hs
@@ -5,7 +5,10 @@ import System.Exit (exitFailure, exitSuccess)
 
 memDev = newMemoryBlockDevice 4096 1048576
 
-assert x = if x then exitSuccess else exitFailure
+assertEq e v = do
+  v' <- e
+  if v == v' then return ()
+  else exitFailure
 
 main :: IO ()
 main = do
@@ -14,20 +17,20 @@ main = do
 
   insert dev "/value1" 1
   insert dev "/value2" 2
-  bd <- lookup dev "/value1"
-  assert $ bd == 1
-  -- store dev "a" "hah"
-  -- store dev "b" "hahah"
-  -- store dev "c" "hahahaha"
-  -- bd <- load dev "a"
-  -- print bd
-  -- bd <- load dev "b"
-  -- print bd
-  -- bd <- load dev "c"
-  -- print bd
-  -- delete dev "b"
-  -- store dev "d" "hohoh"
-  -- bd <- load dev "d"
-  -- print bd
 
-  -- print "\nFinish"
+  assertEq (lookup dev "/value1") 1
+  assertEq (lookup dev "/value2") 2
+
+  store dev "a" "hah"
+  store dev "b" "hahah"
+  store dev "c" "hahahaha"
+
+  assertEq (load dev "a") "hah"
+  assertEq (load dev "b") "hahah"
+  assertEq (load dev "c") "hahahaha"
+
+  delete dev "b"
+  store dev "d" "hohoh"
+  assertEq (load dev "d") "hohoh"
+
+  exitSuccess
